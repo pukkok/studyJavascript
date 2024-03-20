a7e4baaeb53852119ed569991ef05581cd6cfd83
Pukkok <krystalgggg@gmail.com>
2024-03-21 00:12:17 +0900

    프로젝트 시작

--- project/app.js
+++ project/app.js
@@ -23,22 +23,33 @@ const makeHeaderList = () => {
 // makeHeaderList()
 
 const makeSection1Item = () => {
-    const divbox = document.createElement('div')
-    const div = document.createElement('div')
-    div.className = 'img-box'
+    const div1 = document.createElement('div')
+    // div1.innerText= '11'
+    div1.className = 'img-box'
+    const divImg = document.createElement('div')
     const div2 = document.createElement('div')
     div2.className = 'text-box'
-    const ul = document.createElement('ul')
-    for(i=0; i<3; i++){
-        const li = document.createElement('li')
-        const a = document.createElement('a')
-        a.innerText=`${i+1}`
-        li.append(a)
-        ul.append(li)
-    }
-    div2.append(ul)
-    divbox.append(div, div2)
-    section1.append(divbox)
+    const h1 = document.createElement('h1')
+    h1.className = 'title'
+    const h4 = document.createElement('h4')
+    h4.className = 'text'
+    const button = document.createElement('button')
+    button.innerText = 'More'
+    div1.append(divImg)
+    div2.append(h1, h4, button)
+    section1.append(div1, div2)
 }
 makeSection1Item()
-makeSection1Item()
+
+const imgBox = document.querySelector('.img-box')
+const divImg = document.querySelector('.img-box div')
+const startPage = () => {
+    imgBox.style.width = '100%'
+    imgBox.style.height = '100%'
+    divImg.style.bottom = '0'
+    setTimeout(() => {
+        document.body.style.overflowY = 'scroll'
+    }, 500);
+}
+
+window.addEventListener('wheel', startPage)

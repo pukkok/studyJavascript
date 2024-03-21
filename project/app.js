8080a545a8eff393d5eb16afc36b9a6f336ac96e
Pukkok <krystalgggg@gmail.com>
2024-03-21 18:10:27 +0900

    포트폴리오 네브바 마무리하기

--- project/app.js
+++ project/app.js
@@ -1,55 +1,39 @@
+const grid = document.getElementById('grid')
 const root = document.getElementById('root')
-const header = document.createElement('header')
-const main = document.createElement('main')
 
-const section1 = document.createElement('section')
-section1.className = 'section1'
+const header = document.querySelector('header')
+const underLine = document.querySelector('.under-line')
 
-main.append(section1)
-root.append(header, main)
+const mainNav = document.querySelector('.main-nav')
+const mainNavLi = document.querySelectorAll('.product-nav li')
 
-const makeHeaderList = () => {
-    const nav = document.createElement('nav')
-    const ul = document.createElement('ul')
-    for(i=0; i<10; i++){
-        const li = document.createElement('li')
-        const a = document.createElement('a')
-        li.append(a)
-        ul.append(li)
-    }
-    nav.append(ul)
-    header.append(nav)
-}
-// makeHeaderList()
+const depth2 = document.querySelector('header .depth2')
+const depth2Sub = depth2.querySelectorAll('.sub')
+const depth2SubPTag = depth2.querySelectorAll('.sub p')
 
-const makeSection1Item = () => {
-    const div1 = document.createElement('div')
-    // div1.innerText= '11'
-    div1.className = 'img-box'
-    const divImg = document.createElement('div')
-    const div2 = document.createElement('div')
-    div2.className = 'text-box'
-    const h1 = document.createElement('h1')
-    h1.className = 'title'
-    const h4 = document.createElement('h4')
-    h4.className = 'text'
-    const button = document.createElement('button')
-    button.innerText = 'More'
-    div1.append(divImg)
-    div2.append(h1, h4, button)
-    section1.append(div1, div2)
-}
-makeSection1Item()
 
-const imgBox = document.querySelector('.img-box')
-const divImg = document.querySelector('.img-box div')
-const startPage = () => {
-    imgBox.style.width = '100%'
-    imgBox.style.height = '100%'
-    divImg.style.bottom = '0'
-    setTimeout(() => {
-        document.body.style.overflowY = 'scroll'
-    }, 500);
+
+    // underLine.style.width = e.target.offsetWidth + 'px'
+    // underLine.style.left = e.target.offsetLeft + 'px'
+    // underLine.style.top = e.target.offsetHeight + e.target.offsetTop + 'px'
+
+
+
+function openDepth2(){
+    
+    depth2.style.top = '70px'
+    depth2.style.backgroundColor = '#fff'
+    depth2.style.opacity = '1'
 }
 
-window.addEventListener('wheel', startPage)
+const closeDepth2 = () => {
+    depth2.style.top = '-300px'
+    depth2.style.backgroundColor = 'transparent'
+    depth2.style.opacity = '0'
+}
+
+mainNavLi.forEach((li)=>{
+    li.addEventListener('mouseover', openDepth2)
+})
+
+header.addEventListener('mouseleave', closeDepth2)

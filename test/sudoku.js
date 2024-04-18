f2f5ed6b836e6f471a8d5adfce21b166e6c308b7
Pukkok <krystalgggg@gmail.com>
2024-04-18 18:09:24 +0900

    스도쿠 알고리즘 이해하기

--- test/sudoku.js
+++ test/sudoku.js
@@ -1,13 +1,17 @@
 const root = document.getElementById('root')
 
+const box = document.createElement('div')
+box.className = 'sdoku-box'
+const optionBox = document.createElement('div')
+optionBox.className = 'option-box'
+const button = document.createElement('button')
+button.innerText = '다시 뽑기'
+optionBox.append(button)
+let gridSize = 9
+
 function randomNumber(){
   return Math.floor(Math.random()*9)+1
 }
-
-let gridSize = 9
-const box = document.createElement('div')
-box.className = 'sdoku-box'
-
 const standardBox = [
   [0, 0, 0, 1, 1, 1, 2, 2, 2],
   [0, 0, 0, 1, 1, 1, 2, 2, 2],
@@ -21,54 +25,13 @@ const standardBox = [
 ]
 
 let board = JSON.parse(JSON.stringify(standardBox))
-
-board.forEach(line => {
-    line.fill(0)
-})
-
-let matrix1 = []
-let matrix2 = []
-let matrix3 = []
-
-function makeMatrix (min, max, arr){
-    for(let x=min; x<max; x++){
-        for(let y=min; y<max; y++){
-          let num = randomNumber()
-          while(arr.includes(num)){
-            num = randomNumber()
-          }
-          board[x][y] = num
-          arr.push(num)
-        }
-    }
+function boardReset (){
+  board.forEach(line => {
+      line.fill(0)
+  })
 }
+boardReset()
 
-// makeMatrix(0, 3, matrix1)
-// makeMatrix(3, 6, matrix2)
-// makeMatrix(6, 9, matrix3)
-
-// function makeSudoku(board){    
-//     for(let row=0; row<gridSize; row++){
-//         for(let col=0; col<gridSize; col++){
-//             if(board[row][col] === 0){
-//                 for(let num = 1; num <= 9; num++){
-//                     if(isValidSudoku(board, row, col, num)){
-//                         board[row][col] = num
-                        
-//                         if(makeSudoku(board)){
-//                             return true
-//                         }
-                        
-//                         board[row][col] = 0
-//                     }
-                    
-//                 }
-//                 return false
-//             }
-//         }
-//     }
-//     return true
-// }
 let a = []
 function makeSudoku(board){    
     for(let row=0; row<gridSize; row++){
@@ -77,7 +40,7 @@ function makeSudoku(board){
             if(board[row][col] === 0){
                 for(let x = 0; x < 9; x++){
                   
-                    if(isValidSudoku(board, row, col, num)){
+                    if(isValidSudoku(board, row, col, num)){ // 유효성 검사
                         board[row][col] = num
                         
                         if(makeSudoku(board)){
@@ -88,7 +51,7 @@ function makeSudoku(board){
                     }
                   
                 }
-                return false
+                return false // 더이상 돌릴 수 없다.
             }
         }
     }
@@ -99,25 +62,25 @@ function makeSudoku(board){
 function isValidSudoku(board, row, col, num){
   
   // row, col 확인
-  for(let i=0; i<gridSize; i++){
-      if(board[row][i] === num || board[i][col] === num){
-          return false
-      }
-  }
-  
-  // 3 x 3 배열 확인
-  const startRow = Math.floor(row/3)*3
-  const startCol = Math.floor(col/3)*3
-  
-  for(let i= startRow; i<startRow+3; i++){
-      for(let j=startCol; j<startCol+3; j++){
-          if(board[i][j] === num){
-              return false
-          }
-      }
-  }
-  
-  return true
+    for(let i=0; i<gridSize; i++){
+        if(board[row][i] === num || board[i][col] === num){
+            return false
+        }
+    }
+    
+    // 3 x 3 배열 확인
+    const startRow = Math.floor(row/3)*3
+    const startCol = Math.floor(col/3)*3
+    
+    for(let i= startRow; i<startRow+3; i++){
+        for(let j=startCol; j<startCol+3; j++){
+            if(board[i][j] === num){
+                return false
+            }
+        }
+    }
+    
+    return true
 }
 
 makeSudoku(board)
@@ -128,12 +91,15 @@ numberIncludeCheck.forEach(line => {
 })
 console.log(numberIncludeCheck)
 
+let secondCheck = numberIncludeCheck.filter(data => data === 0)
+console.log(secondCheck)
 
 function initNumber () {
   for(let x=0; x<9; x++){
     for(let y=0; y<9; y++){
       const data = document.createElement('div')
       data.className = `data ${y}-${x}`
+      const input = document.createElement('input')
 
       if(x<3){
         if(y<3){
@@ -161,13 +127,21 @@ function initNumber () {
         }
       }
 
-      data.innerText = `${board[x][y]}`
-
+      // data.innerText = `${board[x][y]}`
+      input.value = `${board[x][y]}`
+      data.append(input)
       box.append(data)
     }
   }
   return box 
 }
-
-root.append(box)
 initNumber()
+
+root.append(box ,optionBox)
+
+button.addEventListener('click', ()=>{
+  boardReset()
+  makeSudoku(board)
+  box.innerHTML =''
+  initNumber()
+})
